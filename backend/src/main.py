67fb871d68b36a7f6811fe3819e4bc56ceb0e770
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2025-10-09 16:01:50 +0300

    CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import sign_in_up_routers
 
 app = FastAPI()
 
 app.include_router(sign_in_up_routers.router)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
